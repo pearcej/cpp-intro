2968e3a8334c094be7cae839e9f184f6f3e3da2c
Jan Pearce <pearcej@gmail.com>
2020-07-24 09:40:48 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// This program is designed to introduce C++
+/* multiline comments in
+C++ look like this */
+//comments  to the end of the line in C++ look like this
+
+#include <iostream>   //this is a library which makes inpot & output possible
+using namespace std;  // this is the standard namespace
+
+int main()  // C++ requires a main function and execution begins here
+{       // braces in C++ are used to organize logical blocks of code
+    cout << "Hello world!";   // cout stands for console output
+                              // it is for printing to the screen
+    return 0;   // This is required in the main function, indicating 
+                // that the main() function terminated correctly
+} // This ends the main() function block
